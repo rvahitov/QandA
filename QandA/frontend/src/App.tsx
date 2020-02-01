e9e42301fa69f38159d6cbd13f2c1dbeeda5b7fa
Рамиль Вахитов <rvahitov@outlook.com>
2020-02-01 18:59:04 +0300

    Home page

---
+++ QandA/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
